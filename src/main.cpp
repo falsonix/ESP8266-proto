2d9c6e84e374a4ade79c95de9d20bcbc3a55984c
falsonix <greenemachine124@gmail.com>
2024-11-07 12:03:24 -0700

    change matrix system

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ OTHER LIBRARIES FOR CONNETED DEVICES AND FUNCTIONS
 #include <MD_MAX72xx.h> // include library for control of the MAX7219 (or similar) LED matrices
 
 // define number of LED matrices connected to the system
-#define MAX_DEVICES 1 // should be 7 for one side of the faceplate, 14 for both sides connected
+#define MAX_DEVICES 7 // should be 7 for one side of the faceplate, 14 for both sides connected
 
 // define LED matrix data pins
 #define DATA_PIN D3 // change this to match the data out pin on your board
@@ -23,22 +23,23 @@ OTHER LIBRARIES FOR CONNETED DEVICES AND FUNCTIONS
 // define the MAX7219 object, using mx as the quick reference nickname
 MD_MAX72XX mx = MD_MAX72XX(MD_MAX72XX::FC16_HW, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
-// predefine user specified bitmap image to display (all bitmaps should be 8x8)
-const uint8_t test[8] = { // test bitmap, a lil smiley face :)
-  0b00000000,
-  0b01000010,
-  0b01000010,
-  0b00000000,
-  0b10000001,
-  0b10000001,
-  0b01111110,
-  0b00000000
+// define bitmaps to send to displays (in order)
+const uint8_t bitmaps[MAX_DEVICES][8] = {
+  {0b00000000, 0b01000010, 0b01000010, 0b00000000, 0b10000001, 0b10000001, 0b01111110, 0b00000000}, // test1
+  {0b00000000, 0b01100110, 0b01100110, 0b00000000, 0b10011001, 0b10011001, 0b10011001, 0b01100110}, // test2
+  {0b00000000, 0b01000010, 0b01000010, 0b00000000, 0b10000001, 0b10000001, 0b01111110, 0b00000000}, // test1
+  {0b00000000, 0b01100110, 0b01100110, 0b00000000, 0b10011001, 0b10011001, 0b10011001, 0b01100110}, // test2
+  {0b00000000, 0b01000010, 0b01000010, 0b00000000, 0b10000001, 0b10000001, 0b01111110, 0b00000000}, // test1
+  {0b00000000, 0b01100110, 0b01100110, 0b00000000, 0b10011001, 0b10011001, 0b10011001, 0b01100110}, // test2
+  {0b00000000, 0b01000010, 0b01000010, 0b00000000, 0b10000001, 0b10000001, 0b01111110, 0b00000000} // test1
 };
 
 // define function to send bitmaps to the MAX7219 register
-void sendBMP(const uint8_t bitmap[]) {
-  for (int row = 0; row < 8; row++) {
-    mx.setRow(0, row, bitmap[row]); // for every row, send the bitmap data for that row
+void sendBMP(const uint8_t bitmaps[MAX_DEVICES][8]) {
+  for (int device = 0; device < MAX_DEVICES; device++) {
+    for (int row = 0; row < 8; row++) {
+      mx.setRow(device, row, bitmaps[device][row]);
+    }
   }
 }
 
@@ -49,7 +50,7 @@ void setup() {
   delay(100); // small delay to allow time for the matrix drivers to catch up
   mx.clear(); // clear any content from pre-reset operation
   mx.control(MD_MAX72XX::INTENSITY, 7); // set display brightness to 7, there is probably a shorter function for this
-  sendBMP(test); // display test bitmap 
+  sendBMP(bitmaps);
   mx.transform(MD_MAX72XX::TFUD); // flip bitmap images over, comment this out if your image is upside down
 }
 
